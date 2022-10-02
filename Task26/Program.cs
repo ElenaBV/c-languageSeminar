cb1baf68f29adf16c93b43949907722588ed6771
Elena <bespalova.e.v@mail.ru>
2022-10-03 01:43:32 +0300

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,19 @@
+﻿// число на вход, на выход количество цифр в числе
+
+Console.WriteLine("число  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int QuintityOfNumber(int num)
+{
+    int i = 0;
+    while (num  > 0)
+    {
+        int result = num/10;
+        i++;
+        num = result;
+        
+    }
+return i;
+}
+int result = QuintityOfNumber(number);
+Console.WriteLine ($"{result}");
