0094dcd306de56bfbe072732feda5c25e3cc5ae9
Elena <bespalova.e.v@mail.ru>
2022-10-03 00:21:55 +0300

    HW19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -3,16 +3,26 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+// найти каждую цифру и сравнивать 1 и 5 2 и4 если 1=5 2=4  деление и остаток от деления //сделать реверсию 
 
 Console.WriteLine("Введите пятизначное число ");
-int num = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
-void palindrome(int number)
+int palindrome(int number)
 {
-    int counter = 0;
-    while (counter < number/2)
-    {
-        if i[0] = i[Array.Lenght - 1]
-    }
-    counter++;
+    int lastDigit = number % 10;
+    int firstDigit = number / 10000;
+    int secondDigit = (number / 1000 % 10);
+    int preLastDigit = (number / 10) % 10;
+    int middleDigit = (number / 100) % 10;
+    return (lastDigit * 10000) + (preLastDigit * 1000) + (middleDigit * 100) + (secondDigit * 10) + (firstDigit * 1);
 }
+int result = palindrome(number);
+
+if (result == number) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+
+
+
+
+
