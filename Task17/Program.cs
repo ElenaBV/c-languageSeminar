be06b1aa0b31a5344c48a0e01e57961d31da210d
Elena <bespalova.e.v@mail.ru>
2022-10-02 18:06:15 +0300

    task17

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,10 +1,21 @@
-﻿int num = 13;
-int count = 1;
-while (count <= num)
+﻿// на вход - координаты ХУ ХУ!=0 и выдает номер четверти плоскости в которой находится эта точка 
+
+Console.WriteLine("Введите координаты точки ");
+Console.Write("X:  ");
+int xC = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y:  ");
+int yC = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(xC, yC);
+Console.WriteLine(quarter == 0 ? "Некоректные координаты  " : quarter);
+
+int Quarter(int x, int y)
 {
-    if (count % 2 == 0)
-    {
-        Console.Write($"{count}  ");
-    }
-    count++;
+    if (x > 0 && y > 0) return 1;
+
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+
 }
