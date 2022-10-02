464ac9743cbe73fd001a5fefbba97a9e08925a31
Elena <bespalova.e.v@mail.ru>
2022-10-03 02:18:12 +0300

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// введите число N произведение от1 до N
+
+Console.WriteLine("число  ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 1)
+{
+    int result = DigitMultiplication(number);
+    Console.WriteLine($"{result}");
+}
+else Console.WriteLine("wrong");
+
+int DigitMultiplication ( int num)
+{
+    int multiplic = 1;
+for (int i = 1; i <= num ; i++)
+{
+    multiplic = multiplic * i;
+}
+return multiplic;
+}
