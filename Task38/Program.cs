4a3032e33a307d4b17e95cb763c22cc9920b7cdc
Elena <bespalova.e.v@mail.ru>
2022-10-04 14:42:30 +0300

    HW38

---
+++ Task38/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+double[] CreateArrayRndInt(int size, int min, int max)
+{
+    double[] array = new double[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
+double[] MinMaxArray(double[] array)
+{
+    double maxA = array[0];
+    double minA = array[0];
+
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > maxA) maxA = array[i];
+        else if (array[i] < minA) minA = array[i];
+
+    }
+
+    return new double[] { maxA, minA };
+
+
+}
+
+
+
+double[] array = CreateArrayRndInt(4, 0, 3);
+PrintArray(array);
+
+double[] minMaxArray = MinMaxArray(array);
+foreach (double value in array) Console.WriteLine(Math.Round(value, 2, MidpointRounding.AwayFromZero));
+foreach (double value in array) Console.WriteLine($"Максимальное= {minMaxArray[0]}, Минимальное = {minMaxArray[1]}",
+Math.Round(value, 2, MidpointRounding.AwayFromZero));
+
