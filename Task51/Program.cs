096b1107a17033ad0e94f8abffafbbcbb3a9d4ad
Elena <bespalova.e.v@mail.ru>
2022-10-09 17:25:33 +0300

    task51

---
+++ Task51/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int[,] CreateMatrixSumIJ(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) //rows (0)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //columns (1)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int SumElementsOfMainDiagonal(int[,] matrix)
+{
+    int count = default;
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            if(i == j)count = matrix[i,j] + count;
+        }
+    }
+    return count;
+}
+
+
+int[,] array2d = CreateMatrixSumIJ(3, 4);
+PrintMatrix(array2d);
+Console.WriteLine();
+int sumElementsOfMainDiagonal =SumElementsOfMainDiagonal (array2d);
+Console.WriteLine("Сумма элементов, находящихся на главной диагонали   " + sumElementsOfMainDiagonal);
+
