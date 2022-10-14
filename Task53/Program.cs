ffe4044b883556b3166353bfdc782cb185c6734f
Elena <bespalova.e.v@mail.ru>
2022-10-14 15:38:55 +0300

    task53

---
+++ Task53/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив Напишите программу, которая поменяет первую и последнюю строку массив
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) //rows (0)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //columns (1)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+void ChangeRows(int[,] matrix)
+{
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+        
+    }
+
+}
+
+
+int[,] matrix2d = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(matrix2d);
+Console.WriteLine();
+ChangeRows(matrix2d);
+PrintMatrix(matrix2d);
